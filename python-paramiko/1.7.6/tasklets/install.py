73d28098a285bdef953d9b3536f0d4bbb8865899
deboeckj <devnull@localhost>
2011-04-18 15:42:40 +0200

    Updated install tasklets.

--- python-paramiko/1.7.6/tasklets/install.py
+++ python-paramiko/1.7.6/tasklets/install.py
@@ -1,17 +1,7 @@
 
-__author__ = 'aserver'
+__author__ = 'incubaid'
 __tags__   = 'install',
 
 def main(q, i, params, tags):
     qpackage = params['qpackage']
-    qpackage.copyFiles()
-    #q.platform.ubuntu.check()
-    #from pylabs.Shell import *
-    #ipshell()
-    #q.platform.ubuntu.updatePackageMetadata()
-    #q.platform.ubuntu.checkInstall("eric","eric")
-    #q.platform.ubuntu.install("python-qt4-sql")
-    #path=qpackage.getPathFiles()
-    #path=q.system.fs.joinPaths(path,"rootfs")
-    #q.system.fs.copyDirTree(path, "/")
-
+    q.platform.ubuntu.install(qpackage.name)
