acc27cd074bf9bc219b66c18f5e47e90f3137408
Jo De Boeck <jdeboeck@aserver.com>
2011-04-21 19:06:58 +0200

    We do not want the ubuntu ejabberd started version

--- ejabberd/2.1.5/tasklets/install.py
+++ ejabberd/2.1.5/tasklets/install.py
@@ -5,3 +5,8 @@ __tags__   = 'install',
 def main(q, i, params, tags):
     qpackage = params['qpackage']
     q.platform.ubuntu.install(qpackage.name)
+
+    q.platform.ubuntu.install(qpackage.name)
+    q.platform.ubuntu.stopService(qpackage.name)
+    q.platform.ubuntu.disableStartAtBoot(qpackage.name)
+
