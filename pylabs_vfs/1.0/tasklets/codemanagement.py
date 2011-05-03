7f9ae9faf0f4c949e8731606844030c12f7a05c4
Amir Beshay <amirbeshay@gmail.com>
2011-05-03 17:28:24 +0200

    Adding pylabs_vfs package

---
+++ pylabs_vfs/1.0/tasklets/codemanagement.py
@@ -0,0 +1,23 @@
+
+__author__ = 'aserver'
+__tags__   = 'codemanagement',
+
+def main(q, i, params, tags):
+    from clients.mercurial.HgRecipe import HgRecipe
+    recipe = HgRecipe()
+    #Create a connection to the bitbucket repo
+    connection = i.config.clients.mercurial.findByUrl('https://bitbucket.org/incubaid/pylabs-core')
+    #Add the repo to the recipe
+    recipe.addRepository(connection)
+
+    qpackage = params['qpackage']
+    #Define an export directory in tmp dir to clone the repo in
+    exportdir = q.system.fs.joinPaths(q.dirs.tmpDir, qpackage.name, qpackage.version)
+    #Make sure the export dir is empty before cloning
+    q.system.fs.removeDirTree(exportdir)
+    
+    #Add source to the recipe with the directory from hg repo code you want to put with the full path you need this code to be copied to in the sandbox
+    recipe.addSource(connection, '_beta/apps/pylabs_vfs', q.system.fs.joinPaths(exportdir, 'extensions', 'pylabs_vfs'))
+    q.system.fs.removeDirTree(q.system.fs.joinPaths(exportdir, 'extensions', 'pylabs_vfs', '_archive'))
+    q.system.fs.removeDirTree(q.system.fs.joinPaths(exportdir, 'extensions', 'pylabs_vfs', 'vfsExtension'))
+    recipe.executeTaskletAction(params['action'])
