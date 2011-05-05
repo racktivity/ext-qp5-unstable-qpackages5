5f839e620f0a6b670c958c3b4372f7f3253a955e
Amir Beshay <amir@aserver>
2011-05-05 16:20:14 +0200

    install tasklets now enable extensions for googleCalendar and sugarsync

--- sugarsync/1.0/tasklets/install.py
+++ sugarsync/1.0/tasklets/install.py
@@ -6,5 +6,7 @@ def main(q, i, params, tags):
     qpackage = params['qpackage']
     #Copy files from the qpackage directory to the sandbox real directory destination
     q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'linux64',  'extensions', 'clients', 'sugarsync'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'clients', 'sugarsync'))
+    q.extensions.enable('q.clients.sugarsync')
+    q.extensions.enable('q.clients.sugarsyncapi')
     #Add signal for configuration so that the configure tasklet gets executed after qshell is being restarted after installation
     #qpackage.signalConfigurationNeeded() 
