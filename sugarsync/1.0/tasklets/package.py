9590bc85ac107e356ebd308aeccf32eeb1763f2d
Amir Beshay <amirbeshay@gmail.com>
2011-05-05 13:50:40 +0200

    sugarsync package

---
+++ sugarsync/1.0/tasklets/package.py
@@ -0,0 +1,10 @@
+
+__author__ = 'aserver'
+__tags__ = 'package',
+
+def main(q, i, params, tags):
+    qpackage = params["qpackage"]
+    exportdir = q.system.fs.joinPaths(q.dirs.tmpDir, qpackage.name, qpackage.version)
+    #Copy files from the tmp export dir to the qpackages dir
+    q.system.fs.copyDirTree(exportdir, q.system.fs.joinPaths(qpackage.getPathFiles(), 'linux64'))
+    
