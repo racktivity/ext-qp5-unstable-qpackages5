455c10a60fff1fb467409b5794f369859ed9a072
Amir Beshay <amir@aserver>
2011-05-05 16:17:19 +0200

    added enabling the extension to vfs_extension install tasklet

--- vfs_extension/1.0/tasklets/install.py
+++ vfs_extension/1.0/tasklets/install.py
@@ -6,5 +6,6 @@ def main(q, i, params, tags):
     qpackage = params['qpackage']
     #Copy files from the qpackage directory to the sandbox real directory destination
     q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'linux64',  'extensions', 'clients', 'VFS_extension'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'clients', 'VFS_extension'))
+    q.extensions.enable('q.cloud.system.vfs')
     #Add signal for configuration so that the configure tasklet gets executed after qshell is being restarted after installation
     #qpackage.signalConfigurationNeeded() 
