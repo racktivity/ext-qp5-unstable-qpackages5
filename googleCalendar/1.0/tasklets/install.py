52319eea557c461f8bedaedf9265c386bde834b9
Amir Beshay <amir@aserver>
2011-05-05 17:56:17 +0200

    install tasklets modified

--- googleCalendar/1.0/tasklets/install.py
+++ googleCalendar/1.0/tasklets/install.py
@@ -6,6 +6,6 @@ def main(q, i, params, tags):
     qpackage = params['qpackage']
     #Copy files from the qpackage directory to the sandbox real directory destination
     q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'linux64',  'extensions', 'clients', 'googleCalendar'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'clients', 'googleCalendar'))
-    q.extensions.enable('q.clients.googleCalendar')
+    #q.extensions.enable('q.clients.googleCalendar')
     #Add signal for configuration so that the configure tasklet gets executed after qshell is being restarted after installation
     #qpackage.signalConfigurationNeeded() 
