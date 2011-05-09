f56c6551d124f1b2fd72ab98ae8f533dac40c016
Amir Beshay <amirbeshay@gmail.com>
2011-05-09 18:07:11 +0200

    update fuse_vfs configuration tasklet

--- fuse_vfs/1.0/tasklets/configure.py
+++ fuse_vfs/1.0/tasklets/configure.py
@@ -7,15 +7,16 @@ def main(q, i, params, tags):
     configPath = q.system.fs.joinPaths(q.dirs.cfgDir, 'vfs.cfg')
 
     if not q.system.fs.exists(configPath):
-        configFile = q.tools.iniFile.new(configPath)
+        configFile = q.tools.inifile.new(configPath)
     else:
-        overWrite = q.console.askYesNo('Overwrite existing configuration?')
+        overWrite = q.console.askYesNo('\nOverwrite existing configuration?')
     if overWrite:
+        configFile = q.tools.inifile.open(configPath)
         configFile.addSection('status')
         configFile.addParam('status', 'mounted', 'False')
         
         configFile.addSection('vfs_paths')
-        configFile.addParam('vfs_paths', 'root', q.console.askString('Please enter a default location for the root of the virtual file system'))
+        configFile.addParam('vfs_paths', 'root', q.console.askString('\nPlease enter a default location for the root of the virtual file system'))
         configFile.addParam('vfs_paths', 'metadatapath', q.console.askString('Please enter a default location for the metadata of the virtual file system'))
         configFile.addParam('vfs_paths', 'localfilestore', q.console.askString('Please enter a default location for large files'))
         configFile.addParam('vfs_paths', 'mountpoint', q.console.askString('Please enter a default location for the mountpoint of the virtual file system'))
