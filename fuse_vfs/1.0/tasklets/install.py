d8b08ad400397dd87fcfe84f1b7afc4294c6f59d
Amir Beshay <amirbeshay@gmail.com>
2011-05-11 11:18:11 +0200

    removing old packages

--- fuse_vfs/1.0/tasklets/install.py
+++
@@ -1,9 +0,0 @@
-
-__author__ = 'aserver'
-__tags__   = 'install',
-
-def main(q, i, params, tags):
-    qpackage = params['qpackage']
-    #Copy files from the qpackage directory to the sandbox real directory destination
-    q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'linux64',  'extensions', 'fuse_vfs'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'fuse_vfs'))
-    qpackage.signalConfigurationNeeded() 
