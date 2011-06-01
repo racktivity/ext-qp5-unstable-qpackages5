ec16c07f29837d745c82542aea24fa109fa2baf1
Jo De Boeck <jdeboeck@aserver.com>
2011-06-01 23:45:39 +0200

    Ejabberd starts slowly

--- ejabberd/2.1.5/tasklets/install.py
+++ ejabberd/2.1.5/tasklets/install.py
@@ -1,10 +1,11 @@
 
 __author__ = 'incubaid'
 __tags__   = 'install',
-
+import time
 def main(q, i, params, tags):
     qpackage = params['qpackage']
     q.platform.ubuntu.install(qpackage.name)
+    time.sleep(2)
     q.platform.ubuntu.stopService(qpackage.name)
     q.platform.ubuntu.disableStartAtBoot(qpackage.name)
 
