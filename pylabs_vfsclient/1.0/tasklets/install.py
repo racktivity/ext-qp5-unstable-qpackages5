08b4ef1c63ad94db994549fb38c4afd122191122
Alei eldin Salem <aleieldin.salem@aserver.com>
2011-06-04 17:02:42 +0300

    The new pylabs_vfsclient package

---
+++ pylabs_vfsclient/1.0/tasklets/install.py
@@ -0,0 +1,9 @@
+
+__author__ = 'aserver'
+__tags__   = 'install',
+
+def main(q, i, params, tags):
+    qpackage = params['qpackage']
+    #Copy files from the qpackage directory to the sandbox real directory destination
+    q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'generic',  'extensions', 'pylabs_vfsclient'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'pylabs_vfsclient'))
+
