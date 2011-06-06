4591f093a3c7510e858d92dda736cc4f8dd847ba
Alei eldin Salem <aleieldin.salem@gmail.com>
2011-06-06 18:59:23 +0300

    Getting the code from pylabs-core

---
+++ pylabs_diff/1.2/tasklets/install.py
@@ -0,0 +1,12 @@
+
+__author__ = 'aserver'
+__tags__   = 'install',
+
+def main(q, i, params, tags):
+    qpackage = params['qpackage']
+    #Copy files from the qpackage directory to the sandbox real directory destination
+    q.system.fs.copyDirTree(q.system.fs.joinPaths(qpackage.getPathFiles(),'generic',  'extensions', 'pylabs_diff'), q.system.fs.joinPaths(q.dirs.baseDir, 'lib', 'pylabs', 'extensions', 'pylabs_diff'))
+    #Add signal for configuration so that the configure tasklet gets executed after qshell is being restarted after installation
+    #qpackage.signalConfigurationNeeded()
+    #enabling the package
+    #q.extensions.enable('q.system.vfs')
