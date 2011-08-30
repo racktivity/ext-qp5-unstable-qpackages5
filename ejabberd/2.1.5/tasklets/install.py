405252ac8bfbf028780239938ed8962ab19191ce
Jo De Boeck <jdeboeck@aserver.com>
2011-08-30 22:49:49 +0200

    Let see if this helps

--- ejabberd/2.1.5/tasklets/install.py
+++ ejabberd/2.1.5/tasklets/install.py
@@ -1,12 +1,8 @@
 
 __author__ = 'incubaid'
 __tags__   = 'install',
-import time
 def main(q, i, params, tags):
     qpackage = params['qpackage']
     q.platform.ubuntu.install(qpackage.name)
-    while not q.system.net.checkListenPort(5222):
-        time.sleep(0.5)
-    q.platform.ubuntu.stopService(qpackage.name)
     q.platform.ubuntu.disableStartAtBoot(qpackage.name)
 
